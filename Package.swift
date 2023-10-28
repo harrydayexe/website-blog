31705464f1870e7474bed86655546c3b012a8996
Harry Day <contact@harryday.xyz>
2023-10-28 14:06:53 +0100

    Rename package

--- Package.swift
+++ Package.swift
@@ -3,23 +3,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Harry Day Blog",
+    name: "HarryDayBlog",
     platforms: [.macOS(.v12)],
     products: [
         .executable(
-            name: "Harry Day Blog",
-            targets: ["Harry Day Blog"]
+            name: "HarryDayBlog",
+            targets: ["HarryDayBlog"]
         )
     ],
     dependencies: [
-//        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.9.0"),
-        .package(name: "Publish", url: "https://github.com/harrydayexe/publish.git", from: "0.9.1"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.9.0"),
+//        .package(name: "Publish", url: "https://github.com/harrydayexe/publish.git", from: "0.9.1"),
 //        .package(path: "~/Developer/Publish"),
         .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0")
     ],
     targets: [
         .executableTarget(
-            name: "Harry Day Blog",
+            name: "HarryDayBlog",
             dependencies: ["Publish", "SplashPublishPlugin"]
         )
     ]
